7de95847c454b1508b491e1e37d7112f6bea821b
SaintsConnor <112587648+SaintsConnor@users.noreply.github.com>
2022-09-04 15:07:57 +0100

    Auto Installs Cryptex

--- installgithub.sh
+++ installgithub.sh
@@ -9,4 +9,4 @@ function call_cmd(){
 
 call_cmd "git clone https://github.com/SaintsConnor/Exploits"
 call_cmd "git clone https://github.com/SaintsConnor/Scripts-PenTest"
-
+call_cmd "./Scripts-Pentest/Cryptex/install.sh"
