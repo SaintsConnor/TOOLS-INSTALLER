f6e034f2f88effd99546047751a463c327f1b303
SaintsConnor <112587648+SaintsConnor@users.noreply.github.com>
2022-09-04 15:05:51 +0100

    Add files via upload

---
+++ installgithub.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+function call_cmd(){
+	xte "str $1"
+	sleep 0.5
+	xte "key Return"
+}
+
+call_cmd "git clone https://github.com/SaintsConnor/Exploits"
+call_cmd "git clone https://github.com/SaintsConnor/Scripts-PenTest"
+
