6893e60c5f28caa4994def252ec7331561d43879
SaintsConnor <112587648+SaintsConnor@users.noreply.github.com>
2022-09-04 15:20:25 +0100

    Update installgithub-kali.sh

--- installgithub-kali.sh
+++ installgithub-kali.sh
@@ -9,5 +9,6 @@ function call_cmd(){
 
 call_cmd "git clone https://github.com/SaintsConnor/Exploits"
 call_cmd "git clone https://github.com/SaintsConnor/Scripts-PenTest"
+call_cmd "git clone https://github.com/danielmiessler/SecLists.git"
 call_cmd "git clone https://github.com/SSGorg/Cryptex"
 call_cmd "./Cryptex/src/install.sh"
